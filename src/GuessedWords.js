782c5174b398e2681c59f9c04f0c07f294eb905b
David <dhahn123@gmail.com>
2020-11-01 14:57:46 -0800

    Create guessedWords passing tests

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,8 +2,18 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 const GuessedWords = (props) => {
+    let contents
+    if (props.guessedWords.length === 0) {
+        contents = (
+            <span data-test="guess-instructions">
+                Try to guess the secret word!
+            </span>
+        )
+    }
     return (
-        <div />
+        <div data-test="component-guessed-words">
+            { contents }
+        </div>
     );
 };
 
