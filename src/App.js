053cc5d419aa97a6f2b1f5f5181130b927adb21b
David <dhahn123@gmail.com>
2020-11-01 15:23:26 -0800

    add bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Congrats from './Congrats';
+import GuessedWords from './GuessedWords';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Guessed Word Game</h1>
+        <Congrats success={true} />
+        <GuessedWords guessedWords={[
+          {guessedWord: 'train', letterMatchCount: 3}
+          ]} />
+      </div>
+    );
+  }
 }
 
 export default App;
