24616c54007e0fac4d80b10744825eb9af802d65
David <dhahn123@gmail.com>
2020-11-01 18:40:07 -0800

    Complete input component render tests for success=false

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,6 +1,6 @@
 import { shallow, ShallowWrapper } from 'enzyme';
 import React from 'react';
-import { storeFactory } from '../test/testUtils';
+import { findByTestAttr, storeFactory } from '../test/testUtils';
 import Input from './Input';
 
 /**
@@ -17,15 +17,23 @@ const setup = (initialState={}) => {
 }
 
 describe('render', () => {
-    describe('word has not been guessed', () => {
+        describe('word has not been guessed', () => {
+        let wrapper;
+        beforeEach(() => {
+            const initialState = { success: false };
+            wrapper = setup(initialState);
+        })
         test('renders component without error', () => {
-
+            const component = findByTestAttr(wrapper, "component-input");
+            expect(component.length).toBe(1);
         });
         test('renders input box', () => {
-
+            const inputBox = findByTestAttr(wrapper, "input-box");
+            expect(inputBox.length).toBe(1);
         });
         test('renders submit button', () => {
-
+            const submitButton = findByTestAttr(wrapper, "submit-button");
+            expect(submitButton.length).toBe(1);
         });
     });
     describe('word has been guessed', () => {
