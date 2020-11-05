5c59a345d9f50d67cc9b508befb5c7db61f2d00a
David <dhahn123@gmail.com>
2020-11-04 21:17:19 -0800

    Connect App component (and tests)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getSecretWord } from './actions';
 import Congrats from './Congrats';
 import GuessedWords from './GuessedWords';
+import Input from './Input';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
         <h1>Guessed Word Game</h1>
-        <Congrats success={true} />
-        <GuessedWords guessedWords={[
-          {guessedWord: 'train', letterMatchCount: 3}
-          ]} />
+        <Congrats success={this.props.success} />
+        <Input />
+        <GuessedWords guessedWords={this.props.guessedWords} />
       </div>
     );
   }
 }
 
-export default App;
+  const mapStateToProps = (state) => {
+    const { success, guessedWords, secretWord } = state;
+    return { success, guessedWords, secretWord };
+}
+
+export default connect(mapStateToProps, { getSecretWord })(App);
