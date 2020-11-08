08de053ef3bc788593ead475d33641deb70a3e1a
David <dhahn123@gmail.com>
2020-11-07 22:12:01 -0800

    code successReducer,successReducer test

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,16 +1,14 @@
-/**
- * @function successReducer
- * @param {array} state - Array of guessed words.
- * @param {object} action - action to be reduced.
- * @returns {boolean} - new success state.
- */
+import { actionTypes } from '../actions';
 
-import { actionTypes } from "../actions";
 
 export default (state=false, action) => {
     switch(action.type){
         case (actionTypes.CORRECT_GUESS):
             return true;
+        // Challenge #2: Reset Game
+        case (actionTypes.RESET_GAME):
+            return false;
+        // END: Challenge #2: Reset Game
         default:
             return state;
     }
--- src/reducers/successReducer.test.js
+++ src/reducers/successReducer.test.js
@@ -1,6 +1,6 @@
 import { actionTypes } from '../actions';
 import successReducer from './successReducer';
-// import { actionTypes } from '../actions';
+
 test('returns default initial state of `false` when no action is passed', () => {
     const newState = successReducer(undefined, {});
     expect (newState).toBe(false);
@@ -9,3 +9,12 @@ test('returns state of true upon receiving an action of type `CORRECT_GUESS`', (
     const newState = successReducer(undefined, { type: actionTypes.CORRECT_GUESS });
     expect(newState).toBe(true);
 });
+
+// Challenge #2: Reset Game
+test('returns state of false upon receiving an action type `RESET_GAME`', () => {
+    // start with success true, since success is false by default
+    const newState = successReducer(true, { type: actionTypes.RESET_GAME });
+    expect(newState).toBe(false);
+});
+
+// END: Challenge #2: Reset Game
